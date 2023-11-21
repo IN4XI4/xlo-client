d7e30a9752f2dc602b08c70edd704aa5a6a6f811
juan <juankrios15@gmail.com>
2023-11-21 11:38:38 -0500

    list stories

--- src/components/topics/StoriesList.jsx
+++ src/components/topics/StoriesList.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { getStoriesByTopic } from '../../api/blog.api';
-
+import { FaAngleDown, FaRegSquare} from 'react-icons/fa';
 
 
 export function StoriesList({ topicId }) {
@@ -23,16 +23,37 @@ export function StoriesList({ topicId }) {
         }
     }
     return (
-        <div className='bg-white border rounded p-2 text-gray-500'>
-            <div className='grid grid-cols-10'>
+        <div className='bg-white border rounded p-3 text-gray-500'>
+            <div className='grid grid-cols-10 pb-3'>
                 <div className='col-span-7 flex'>
                     <button className='mr-3'>Stories</button>
                     <button className='text-[#3DB1FF] underline'>Latest</button>
                 </div>
-                <div>Replies</div>
-                <div>Views</div>
-                <div>Likes</div>
+                <div className='flex justify-center items-center'>Replies <span><FaAngleDown /></span></div>
+                <div className='flex justify-center items-center'>Views <span><FaAngleDown /></span></div>
+                <div className='flex justify-center items-center'>Likes <span><FaAngleDown /></span></div>
             </div>
+            {stories.map((story, index) => (
+                <div key={index} className='grid grid-cols-10 py-3'>
+                    <div className='col-span-7 bg-gray-50 p-3 rounded-lg'>
+                        <div className='font-bold text-black text-xl'>{story.title}</div>
+                        <div className='flex justify-between'>
+                            <div className='text-sm'>{story.subtitle}</div>
+                            <div className='flex'>
+                                {story.card_colors.map((color, colorIndex) => (
+                                    <div key={colorIndex} className="pe-1" style={{ color: color|| "#3DB1FF" }}>
+                                        <FaRegSquare />
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                    <div className='text-center text-[#3DB1FF] self-center'>{story.comments_count}</div>
+                    <div className='text-center text-[#3DB1FF] self-center'>{story.views_count}</div>
+                    <div className='text-center text-[#3DB1FF] self-center'>{story.likes_count}</div>
+                </div>
+            ))}
+            {error && <p className='text-red-500'>Error loading stories: {error.message}</p>}
         </div>
     )
 }
