26f6a680e946740e3d2f9adcb4490ead767db980
juan <juankrios15@gmail.com>
2024-03-02 12:09:59 -0500

    fixed profile modal size

--- src/components/ProfileModal.jsx
+++ src/components/ProfileModal.jsx
@@ -30,7 +30,7 @@ export function ProfileModal({ userId, onClose }) {
   if (!isOpen) return null;
   return (
     <div className="fixed inset-0 bg-gray-300 bg-opacity-50 overflow-y-auto h-full w-full" onClick={onClose}>
-      <div id="principal" className="relative top-32 mx-auto px-6 pt-4 border w-80 md:w-1/2 lg:w-1/3 xl:w-1/4 shadow-lg rounded-md bg-white" onClick={handleModalClick}>
+      <div id="principal" className="relative top-32 mx-auto px-6 pt-4 border w-80 md:w-1/2 lg:w-1/3 2xl:w-1/4 shadow-lg rounded-md bg-white" onClick={handleModalClick}>
         <div id="relativo" className='relative -top-8 md:-top-16'>
           <div className='flex items-end border-b pb-3 '>
             <div className='ps-2'>
