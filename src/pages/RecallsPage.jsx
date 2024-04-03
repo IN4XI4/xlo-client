a098a0c46377cff04ffd90abddd5ad55766bbcf7
juan <juankrios15@gmail.com>
2024-04-03 08:56:20 -0500

    go to next or previous card

--- src/pages/RecallsPage.jsx
+++ src/pages/RecallsPage.jsx
@@ -3,6 +3,8 @@ import { FaRegBookmark, FaRegTimesCircle } from 'react-icons/fa'
 import { HiOutlineArrowLeftCircle, HiOutlineArrowRightCircle } from "react-icons/hi2";
 import { getMyRecallCards } from '../api/blog.api';
 import { useLocation, useNavigate } from 'react-router-dom';
+import { BlocksList } from '../components/topics/BlocksList';
+import { getContentTypes } from '../api/base.api';
 
 
 export function RecallsPage() {
@@ -10,6 +12,7 @@ export function RecallsPage() {
   const [error, setError] = useState(null);
   const [isCardsLoaded, setIsCardsLoaded] = useState(false);
   const [currentCardIndex, setCurrentCardIndex] = useState(0);
+  const [blockContentTypeId, setBlockContentTypeId] = useState(null);
   const navigate = useNavigate();
   const location = useLocation();
 
@@ -28,6 +31,35 @@ export function RecallsPage() {
     }
   }
 
+  async function loadContentTypes() {
+    try {
+      const res = await getContentTypes();
+      const contentTypes = res.data;
+      const blockType = contentTypes.find(ct => ct.model === 'block');
+      if (blockType) setBlockContentTypeId(blockType.id);
+    } catch (error) {
+      console.error('Error al cargar los ContentTypes', error);
+    }
+  }
+
+  useEffect(() => {
+    loadContentTypes();
+  }, []);
+
+  const goToNextCard = () => {
+    if (currentCardIndex < recallCards.length - 1) {
+      setCurrentCardIndex(currentCardIndex + 1);
+      window.scrollTo(0, 0);
+    }
+  };
+
+  const goToPreviousCard = () => {
+    if (currentCardIndex > 0) {
+      setCurrentCardIndex(currentCardIndex - 1);
+      window.scrollTo(0, 0);
+    }
+  };
+
   const handleBackClick = () => {
     if (location.state?.fromNavigation) {
       navigate(-1);
@@ -37,13 +69,22 @@ export function RecallsPage() {
   };
 
   return (
-    <div className="pt-20 md:pt-28 px-4 md:px-16 lg:px-32 xl:px-44">
+    <div className="pb-20 pt-24 md:pt-28 px-4 md:px-16 lg:px-32 xl:px-44">
       {!isCardsLoaded ? (
         <div>Loading...</div>
-      ) : recallCards.length === 0 ? (
+      ) : recallCards.length === 0 && currentCardIndex < recallCards.length ? (
         <div>You have no recall cards.</div>
       ) : (
-        <div>{recallCards[currentCardIndex].card.title}</div>
+        <div>
+          <div className='text-xl text-gray-500 pb-3'>
+            {recallCards[currentCardIndex].card.title}
+          </div>
+          <div className='md:px-16 lg:px-24 mb-3'>
+            <BlocksList key={recallCards[currentCardIndex].card.id}
+              card={recallCards[currentCardIndex].card}
+              blockContentTypeId={blockContentTypeId} />
+          </div>
+        </div>
       )}
       <div className="fixed inset-x-0 bottom-0 text-white text-center z-50">
         <div className='bg-[#0098FF] text-end px-4 text-sm'>
@@ -55,10 +96,10 @@ export function RecallsPage() {
           </div>
           <div className='flex-grow items-center flex justify-center space-x-4 '>
             <div>
-              <HiOutlineArrowLeftCircle className='text-4xl' />
+              <HiOutlineArrowLeftCircle className='text-4xl cursor-pointer' onClick={goToPreviousCard} />
             </div>
             <div>
-              <HiOutlineArrowRightCircle className='text-4xl' />
+              <HiOutlineArrowRightCircle className='text-4xl cursor-pointer' onClick={goToNextCard} />
             </div>
           </div>
           <div className='flex-none  flex items-center'>
