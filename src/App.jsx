99e7eb0442e032eea6f06272a647a52d55922e36
juan <juankrios15@gmail.com>
2024-07-27 18:35:47 -0500

    pwa app installation and button

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { BrowserRouter, Routes, Route, Navigate, useLocation } from 'react-router-dom'
 import ReactGA from "react-ga4";
 import { HomePage } from './pages/HomePage'
@@ -15,6 +15,7 @@ import { LearnSoftSkillsPage } from './pages/LearnSoftSkillsPage'
 import { LearningProgramPage } from './pages/LearningProgramPage'
 import { FocusedRecallBlocksPage } from './pages/FocusedRecallBlocksPage';
 import { SparkedRecallBlocksPage } from './pages/SparkedRecallBlocksPage';
+import useBeforeInstallPrompt from './hooks/UseBeforeInstallPrompt';
 
 
 const isProduction = import.meta.env.VITE_ENV === 'production';
@@ -57,17 +58,47 @@ function ProtectedRoute({ children }) {
 function App() {
   const location = useLocation();
   const title = formatTitle(location.pathname);
+  const deferredPrompt = useBeforeInstallPrompt();
+  const [isInstallable, setIsInstallable] = useState(false);
+
   useEffect(() => {
     if (isProduction) {
       ReactGA.send({ hitType: "pageview", page: location.pathname, title: title });
     }
   }, [location]);
 
+  useEffect(() => {
+    if (deferredPrompt) {
+      setIsInstallable(true);
+    }
+  }, [deferredPrompt]);
+
+  const handleInstallClick = async () => {
+    if (deferredPrompt) {
+      console.log('Prompting installation...');
+      deferredPrompt.prompt();
+      const choiceResult = await deferredPrompt.userChoice;
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the install prompt');
+      } else {
+        console.log('User dismissed the install prompt');
+      }
+    } else {
+      console.log('No deferred prompt available');
+    }
+  };
+
   const token = localStorage.getItem("token");
   return (
     <div className="flex flex-col min-h-screen bg-gray-50">
       {token && <Navigation />}
       <div className="flex-grow">
+        {isInstallable && (
+          <button onClick={handleInstallClick} 
+          className="fixed bottom-4 right-4 z-40 p-2 border-2 rounded-lg border-[#3DB1FF] text-[#3DB1FF] shadow bg-white">
+            Install App
+          </button>
+        )}
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/topic/:slug" element={<ProtectedRoute><TopicStoriesPage /></ProtectedRoute>} />
---
+++ src/hooks/UseBeforeInstallPrompt.jsx
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react';
+
+const useBeforeInstallPrompt = () => {
+  const [deferredPrompt, setDeferredPrompt] = useState(null);
+
+  useEffect(() => {
+    const handler = (e) => {
+      e.preventDefault();
+      setDeferredPrompt(e);
+    };
+
+    window.addEventListener('beforeinstallprompt', handler);
+
+    return () => {
+      window.removeEventListener('beforeinstallprompt', handler);
+    };
+  }, []);
+
+  return deferredPrompt;
+};
+
+export default useBeforeInstallPrompt;
